83433b62903cbfca271a8a5be19cf037f1351467
Movle <richard.xiu.520@gmail.com>
2021-03-16 21:20:35 +0800

    mybatis项目-权限管理-新建实体类

---
+++ learning-mybatis/mybatis-02/src/main/java/com/movle/mybatis/simple/domain/SysUser.java
@@ -0,0 +1,79 @@
+package com.movle.mybatis.simple.domain;
+
+import java.util.Date;
+
+/**
+ * @ClassName SysUser
+ * @MethodDesc: 实体类
+ * @Author Movle
+ * @Date 2021/3/16 下午9:16
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+
+
+public class SysUser {
+    private Long id;
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+    private String userInfo;
+    private byte[] headImg;
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(String userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public byte[] getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(byte[] headImg) {
+        this.headImg = headImg;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
