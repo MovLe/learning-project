1b518cd0b1cd795e16ffdea2c30f763bebce26ec
Movle <richard.xiu.520@gmail.com>
2021-03-17 22:16:29 +0800

    mybatis项目-权限管理-@Insert注解的使用

--- learning-mybatis/mybatis-02/src/main/java/com/movle/mybatis/simple/mapper/RoleMapper.java
+++ learning-mybatis/mybatis-02/src/main/java/com/movle/mybatis/simple/mapper/RoleMapper.java
@@ -1,23 +1,12 @@
 package com.movle.mybatis.simple.mapper;
 
 import com.movle.mybatis.simple.domain.SysRole;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 public interface RoleMapper {
-    @Results(id = "roleResultMap",value = {
-            @Result(property = "id",column = "id",id = true),
-            @Result(property = "roleName",column = "role_name"),
-            @Result(property = "enabled",column = "enabled"),
-            @Result(property = "createBy",column = "create_by"),
-            @Result(property = "createTime",column = "create_time")
 
-    })
-//    @ResultMap("roleResultMap")
     @Select({"select id,role_name,enabled,create_by,create_time from sys_role where id = #{id}"})
     SysRole selectById2(Long id);
 
@@ -29,7 +18,20 @@ public interface RoleMapper {
             @Result(property = "createTime",column = "create_time")
 
     })
-    @ResultMap("roleResultMap")
+//    @ResultMap("roleResultMap")
     @Select("select * from sys_role")
-    List<SysRole> selectAll();
+    List<SysRole> annotationselectAll();
+
+    @Insert({"insert into sys_role(id,role_name,enabled,create_by,create_time)","values(#{id},#{roleName},#{enabled},#{createBy},","#{createTime,jdbcType=TIMESTAMP})"})
+    int insert(SysRole sysRole);
+
+    @Insert({"insert into sys_role(id,role_name,enabled,create_by,create_time)","values(#{id},#{roleName},#{enabled},#{createBy},","#{createTime,jdbcType=TIMESTAMP})"})
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    int insert2(SysRole sysRole);
+
+    @Insert({"insert into sys_role(id,role_name,enabled,create_by,create_time)","values(#{id},#{roleName},#{enabled},#{createBy},","#{createTime,jdbcType=TIMESTAMP})"})
+    @SelectKey(statement ="SELECT LAST_INSERT_ID()",keyProperty = "id",resultType = Long.class,before = false)
+    int insert3(SysRole sysRole);
+
+
 }
--- learning-mybatis/mybatis-02/src/main/java/com/movle/mybatis/simple/test/AnnotationUserMapperTest.java
+++ learning-mybatis/mybatis-02/src/main/java/com/movle/mybatis/simple/test/AnnotationUserMapperTest.java
@@ -5,6 +5,7 @@ import com.movle.mybatis.simple.mapper.RoleMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,12 +42,75 @@ public class AnnotationUserMapperTest extends BaseMapperTest{
         try{
             RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
 
-            List<SysRole> sysRoles = roleMapper.selectAll();
+            List<SysRole> sysRoles = roleMapper.annotationselectAll();
 
             for(SysRole s:sysRoles){
                 System.out.println("s:"+s);
             }
 
+        }finally {
+            sqlSession.commit();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testInsert1(){
+        SqlSession sqlSession =getSqlSession();
+        try{
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole sysRole=new SysRole();
+            sysRole.setId(4L);
+            sysRole.setRoleName("普通用户3");
+            sysRole.setEnabled(1);
+            sysRole.setCreateBy(1);
+            sysRole.setCreateTime(new Date());
+
+            int result = roleMapper.insert(sysRole);
+
+            System.out.println("result:"+result);
+        }finally {
+            sqlSession.commit();
+            sqlSession.close();
+        }
+    }
+    @Test
+    public void testInsert2(){
+        SqlSession sqlSession =getSqlSession();
+        try{
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole sysRole=new SysRole();
+            sysRole.setId(5L);
+            sysRole.setRoleName("普通用户4");
+            sysRole.setEnabled(1);
+            sysRole.setCreateBy(1);
+            sysRole.setCreateTime(new Date());
+
+            int result = roleMapper.insert2(sysRole);
+
+            long id = sysRole.getId();
+            System.out.println("result:"+result+" id:"+id);
+        }finally {
+            sqlSession.commit();
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testInsert3(){
+        SqlSession sqlSession =getSqlSession();
+        try{
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole sysRole=new SysRole();
+            sysRole.setRoleName("普通用户4");
+            sysRole.setEnabled(1);
+            sysRole.setCreateBy(1);
+            sysRole.setCreateTime(new Date());
+
+            int result = roleMapper.insert3(sysRole);
+
+            long id = sysRole.getId();
+            System.out.println("result:"+result+" id:"+id);
         }finally {
             sqlSession.commit();
             sqlSession.close();
